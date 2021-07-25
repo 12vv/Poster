ceb758d2822cd12f3d8cd9c7be2c95bcc706280a
garrick <yegarrick12@gmail.com>
2021-07-25 12:58:50 -0600

    deploy

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,14 +1,20 @@
 import express from "express";
 //, getPost,  updatePost, likePost, deletePost
-import { getPosts, createPost } from "../controllers/posts.js";
+import {
+  getPosts,
+  createPost,
+  updatePost,
+  deletePost,
+  likePost,
+} from "../controllers/posts.js";
 
 const router = express.Router();
 
 router.get("/", getPosts);
 router.post("/", createPost);
 // router.get('/:id', getPost);
-// router.patch('/:id', updatePost);
-// router.delete('/:id', deletePost);
-// router.patch('/:id/likePost', likePost);
+router.patch("/:id", updatePost);
+router.delete("/:id", deletePost);
+router.patch("/:id/likePost", likePost);
 
 export default router;
